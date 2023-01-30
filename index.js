9235c05d206058c6ca664d27b55d23d5526bb2f2
Hara Chung <harachung90@gmail.com>
2023-01-30 18:54:54 +1100

    Second commit. All conversion done.

--- index.js
+++ index.js
@@ -7,8 +7,13 @@
 const inputVal = document.getElementById('num_input')
 const convertBtn = document.getElementById('convert-btn')
 
+const lengthP = document.getElementById('length_converted')
+const volumeP = document.getElementById('volume-converted')
+const massP = document.getElementById('mass-converted')
+
 convertBtn.addEventListener('click', function() {
     let val =inputVal.valueAsNumber
-    console.log(val)
-
+    lengthP.textContent = `${val} meters = ${ (3.281 * val).toFixed(3)} feet | ${val} feet = ${ (0.3048 * val).toFixed(3)} meters`
+    volumeP.textContent = `${val} liters = ${ (0.264 * val).toFixed(3)} gallons | ${val} gallons = ${(3.785 * val).toFixed(3)} liters`
+    massP.textContent = `${val} kilos = ${(2.204 * val).toFixed(3)} pounds | ${val} pounds = ${(0.453 * val).toFixed(3)} kilos`
 })
